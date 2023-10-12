1a8abbd74111e869a717ccf97b40db7ff3e01cf7
Nada Anis Nurjihan <nurjinurjihan26@gmail.com>
2023-10-12 22:17:39 +0700

    second

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from flask import Flask, render_template
 # from pymongo import MongoClient
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 # client = MongoClient('mongodb+srv://???:sparta@cluster0.gaybq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 # db = client.dbsparta
